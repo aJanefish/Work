46cadc7b16ed6810a743885fe240cce58455dd0d
zhangyu <yu.zhang@singou.mo>
2018-09-18 10:03:35 +0800

    change demo

---
+++ UI/DrawableDemo/app/src/main/java/launcher/butler/com/drawabledemo/utils/Utils.java
@@ -0,0 +1,19 @@
+package launcher.butler.com.drawabledemo.utils;
+
+public class Utils {
+	/**
+	 * 序号	节点	Drawable 具体子类	说明
+	 * 1	[bitmap]	 BitmapDrawable	对bitmap的封装
+	 * 2	[color]	     ColorDrawable	把颜色封装成可以显示的Drawable
+	 * 3	[shape]      ShapeDrawable	绘制基础的形状图形，最常用的Drawable之一，可用于定义圆角、渐变色、圆环等效果
+	 * 4	[selector]	 StateListDrawable	表示一组有状态的图像的集合，可用于给button等设置点击和正常状态下的图片
+	 * 5	[level-list] LevelListDrawable	表示一组有等级的图像的集合
+	 * 6	[layer-list] LayerDrawable	表示图层
+	 * 7	[nine-patch] NinePatchDrawable	对.9.png的封装
+	 * 8	[clip]       ClipDrawable	对drawable裁剪
+	 * 9	[inset]	     InsetDrawable	用距离边框的距离表示drawable大小
+	 * 10	[ripple]	 RippleDrawable	波纹点击效果，API21以上
+	 
+
+	 * */
+}
