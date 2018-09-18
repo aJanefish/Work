d46eaa9d9bab4bed7fb5226fc33e2f5f21f93025
zhangyu <yu.zhang@singou.mo>
2018-09-18 11:40:39 +0800

    Drawable demo

--- UI/DrawableDemo/app/src/main/java/launcher/butler/com/drawabledemo/MainActivity.java
+++ UI/DrawableDemo/app/src/main/java/launcher/butler/com/drawabledemo/MainActivity.java
@@ -2,6 +2,7 @@ package launcher.butler.com.drawabledemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -10,4 +11,18 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	public void levelListClick(View view) {
+		if (flag) {
+			view.getBackground().setLevel(3);
+		} else {
+			view.getBackground().setLevel(13);
+		}
+		this.flag = !this.flag;
+	}
+	
+	boolean flag;
+
+	
+
 }
