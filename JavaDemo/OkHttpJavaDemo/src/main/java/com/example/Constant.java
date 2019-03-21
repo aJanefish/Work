f3bfb43cbf1680e4f2e4faf3b792df9051bd8260
zhangyu <yu.zhang@singou.mo>
2019-03-21 18:41:04 +0800

    add:okhttp java demo

---
+++ JavaDemo/OkHttpJavaDemo/src/main/java/com/example/Constant.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Constant {
+    public static final String URL = "http://localhost:4567/";
+
+    public static final String BAIDU = "https://www.baidu.com/";
+
+}
---
+++ JavaDemo/OkHttpJavaDemo/src/main/java/com/example/Example01.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import okhttp3.*;
+
+import java.io.IOException;
+import java.rmi.server.RemoteRef;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.Constant.BAIDU;
+import static com.example.Constant.URL;
+
+public class Example01 {
+
+
+
+    public static void main(String args[]) {
+        Request request = new Request.Builder()
+                .url(BAIDU)
+                .build();
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(60, TimeUnit.SECONDS);
+        builder.readTimeout(60, TimeUnit.SECONDS);
+        builder.writeTimeout(60, TimeUnit.SECONDS);
+        OkHttpClient okHttpClient = builder.build();
+
+        Call cell = okHttpClient.newCall(request);
+
+        cell.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                System.out.println(response.body());
+            }
+        });
+
+    }
+}
