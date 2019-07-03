6b45db124937acb49fd4dbbbcae1721e1f48e081
aJanefish <36288075+aJanefish@users.noreply.github.com>
2019-07-03 09:48:50 +0800

    delete some

--- JavaDemo/OkHttpJavaDemo/src/main/java/com/example/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Constant {
-    public static final String LOCALHOST_URL_4567 = "http://localhost:4567/";
-    public static final String LOCALHOST_URL_9999 = "http://localhost:9999/";
-    public static final String BAIDU = "https://www.baidu.com/";
-
-}
---
+++ JavaDemo/OkioDemo/src/main/java/com/test/SocketTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class SocketTest {
+
+    public static void main(String[] args) {
+        Socket socket = new Socket();
+        //InetSocketAddress inetSocketAddress = new InetSocketAddress();
+        //socket.connect();
+    }
+}
